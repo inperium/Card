9c17941b703a85a6ddc30e1014b088593931dbcc
inperium <ibow9416@740s-pl-111.local>
2016-10-25 13:47:20 -0600

    Get & Set

--- Card/Model/PlayingCard.swift
+++ Card/Model/PlayingCard.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2016 Bowen, Isaac. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PlayingCard : Card
+{
+    private var rank : Int
+    private var suit : String
+    private var color : UIColor
+    
+    override init()
+    {
+        rank = 0
+        suit = ""
+        color = UIColor()
+        super.init()
+    }
+    
+    func getRank() -> Int
+    {
+        return rank
+    }
+    
+    func getSuit() -> String
+    {
+        return suit
+    }
+    
+    func getColor() -> UIColor
+    {
+        return UIColor()
+    }
+    
+    override func toString() -> String
+    {
+        let description = "The playing card rank is: \(rank), its suit is \(suit) and the color is \(color)."
+        
+        return description
+    }
+}
