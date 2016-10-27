3076603329f60b6f2d2fd426fd0af6d09d3fcaaa
inperium <ibow9416@740s-pl-111.local>
2016-10-27 12:31:05 -0600

    Changed Groups and deleted files.

---
+++ Card/Controller/CardGameController.swift
@@ -0,0 +1,14 @@
+//
+//  CardGameController.swift
+//  Card
+//
+//  Created by Bowen, Isaac on 10/27/16.
+//  Copyright © 2016 Bowen, Isaac. All rights reserved.
+//
+
+import UIKit
+
+class CardGameController : UIViewController
+{
+    
+}
--- Card/Model/PlayingCard.swift
+++ Card/Model/PlayingCard.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class PlayingCard : Card
 {
-    private var rank : Int
     private var suit : String
+        {
+        get
+        {
+            return self.suit
+        }
+        set(suit)
+        {
+            self.suit = suit
+        }
+    }
     private var color : UIColor
+        {
+        get
+        {
+            return self.color
+        }
+        set(color)
+        {
+            self.color = color
+        }
+    }
+    
+    private var rank : Int
+    {
+        get
+        {
+            return self.rank
+        }
+        set(rank)
+        {
+            self.rank = rank
+        }
+    }
     
     override init()
     {
+
+        super.init()
         rank = 0
         suit = ""
         color = UIColor()
-        super.init()
     }
     
     func getRank() -> Int
@@ -39,6 +71,15 @@ class PlayingCard : Card
     
     override func toString() -> String
     {
+        let backStatus : String
+        if super.isFacing()
+        {
+                backStatus = "face up"
+        }
+        else
+        {
+            backStatus = "face down"
+        }
         let description = "The playing card rank is: \(rank), its suit is \(suit) and the color is \(color)."
         
         return description
