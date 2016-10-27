cbb7ecd76dfa4453edcc4afb8e10b01c457ac863
inperium <ibow9416@740s-pl-111.local>
2016-10-27 11:57:28 -0600

    Added properties to the card data members.

--- Card/Model/Card.swift
+++ Card/Model/Card.swift
@@ -11,7 +11,28 @@ import UIKit
 class Card
 {
     private var backImage : UIImage
+        {
+            get
+            {
+                return self.backImage
+            }
+            set(backImage)
+            {
+                self.backImage = backImage
+            }
+        }
+    
     private var isFaceUp : Bool
+        {
+        get
+        {
+            return self.isFaceUp
+        }
+        set(isFaceUp)
+        {
+            self.isFaceUp = isFaceUp
+        }
+    }
 
     init()
     {
@@ -31,7 +52,7 @@ class Card
     
     func toString() -> String
     {
-        let description = "This card is \(isFaceUp)"
+        let description = "This card is \(isFaceUp) and \(self.getBackImage()) is the image."
         
         return description
     }
