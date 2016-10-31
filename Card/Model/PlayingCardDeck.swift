938c7c9706b9e1c8e73f82d37d94269df9214a02
inperium <ibow9416@740s-pl-111.local>
2016-10-31 12:43:15 -0600

    Classes were created.

--- Card/Model/PlayingCardDeck.swift
+++ Card/Model/PlayingCardDeck.swift
@@ -10,5 +10,19 @@ import UIKit
 
 class PlayingCardDeck : Deck
 {
+        internal lazy var cards = [PlayingCard()]
+    override init()
+    {
+        super.init()
+    }
     
+    func shuffleDeck() -> Void
+    {
+        
+    }
+    
+    func orderDeck() -> Void
+    {
+        
+    }
 }
