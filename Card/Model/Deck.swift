8805df865a15ec524c1bec20535de120e90d0c48
inperium <ibow9416@740s-pl-111.local>
2016-10-31 12:34:03 -0600

    Created classes.

---
+++ Card/Model/Deck.swift
@@ -0,0 +1,14 @@
+//
+//  Deck.swift
+//  Card
+//
+//  Created by Bowen, Isaac on 10/31/16.
+//  Copyright © 2016 Bowen, Isaac. All rights reserved.
+//
+
+import Foundation
+
+class Deck
+{
+    
+}
---
+++ Card/Model/PlayingCardDeck.swift
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.swift
+//  Card
+//
+//  Created by Bowen, Isaac on 10/31/16.
+//  Copyright © 2016 Bowen, Isaac. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardDeck : Deck
+{
+    
+}
